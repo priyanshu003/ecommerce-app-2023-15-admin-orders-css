c5eb1da297581f190a0803f08d7c06957a5c5592
priyanshu jain <jainpriyanshu341@gmail.com>
2023-04-28 12:09:28 +0530

    first

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "RS0 to 500",
+    array: [0, 19],
+  },
+  {
+    _id: 1,
+    name: "Rs500 to 1000",
+    array: [20, 39],
+  },
+  {
+    _id: 2,
+    name: "RS1000 to 1500",
+    array: [40, 59],
+  },
+  {
+    _id: 3,
+    name: "RS1500 to 2000",
+    array: [60, 79],
+  },
+  {
+    _id: 4,
+    name: "RS2000 to 25000",
+    array: [80, 99],
+  },
+  {
+    _id: 4,
+    name: "RS2500 or more",
+    array: [100, 9999],
+  },
+];
